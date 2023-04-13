32ba8fcc0d5600e06f5016639595d73b9143cb37
Rupsy <rupam.shekhar@gmail.com>
2023-04-13 17:42:51 +0530

    Initial commit : Api Testing
    Using Spotify

---
+++ pythonProject/Tests/conftest.py
@@ -0,0 +1,32 @@
+import json
+
+import base64
+from requests import post
+import pytest
+
+
+
+
+client_id = "75019065a1aa4a599c64698ef08eb7fa"
+client_secret = "363e06c1f38f40798373255a88b7c422"
+
+@pytest.fixture( scope="session")
+def get_token():
+    auth_base64 = ""
+    data = ""
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode ( "utf-8" )
+    auth_base64 = str ( base64.b64encode ( auth_bytes ), "utf-8" )
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {"Authorization": "Basic " + auth_base64,
+                   "Content-Type": "application/x-www-form-urlencoded"}
+
+    data = {"grant_type": "client_credentials"}
+
+    result = post ( url, headers=headers, data=data )
+    json_results = json.loads ( result.content )
+    access_token = json_results['access_token']
+    return access_token
+
+
---
+++ pythonProject/Tests/test_123.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_search(get_token):
+   token = get_token
+   assert 3 % 3 == 0
+   print ("PPPPPPPPMMMM", token)
+
